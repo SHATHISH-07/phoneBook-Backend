00a246de92336a3b7125d8a6491452cb3440f668
SHATHISH-07 <satheesh12072004@gmail.com>
2024-11-13 13:41:13 +0530

    uibuild

--- index.js
+++ index.js
@@ -1,139 +1,139 @@
-require("dotenv").config();
-const express = require("express");
-const Person = require("./models/person");
-const morgan = require("morgan");
-const cors = require("cors");
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const Person = require('./models/person')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
 
-app.use(express.json());
-app.use(cors());
-app.use(express.static("dist"));
+app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
 
-morgan.token("postData", (req) => JSON.stringify(req.body));
+morgan.token('postData', (req) => JSON.stringify(req.body))
 
 app.use(
   morgan((tokens, req, res) => {
     const format =
-      req.method === "POST"
-        ? ":method :url :status :res[content-length] - :response-time ms :postData"
-        : ":method :url :status :res[content-length] - :response-time ms";
-    return morgan.compile(format)(tokens, req, res);
+      req.method === 'POST'
+        ? ':method :url :status :res[content-length] - :response-time ms :postData'
+        : ':method :url :status :res[content-length] - :response-time ms'
+    return morgan.compile(format)(tokens, req, res)
   })
-);
+)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malFormatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).send({ error: error.message });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malFormatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
 const internalServerError = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
   // Respond with 500 status code for any error
-  response.status(500).send({ error: "Internal Server Error" });
-  next(error);
-};
+  response.status(500).send({ error: 'Internal Server Error' })
+  next(error)
+}
 
-app.get("/api/persons", async (req, res) => {
+app.get('/api/persons', async (req, res, next) => {
   try {
-    const persons = await Person.find({});
-    res.status(200).json(persons);
+    const persons = await Person.find({})
+    res.status(200).json(persons)
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
-app.get("/api/persons/:id", async (req, res) => {
+app.get('/api/persons/:id', async (req, res, next) => {
   try {
-    const person = await Person.findById(req.params.id);
+    const person = await Person.findById(req.params.id)
     if (person) {
-      res.status(200).json(person);
+      res.status(200).json(person)
     } else {
-      res.status(404).send({ error: "person not found" });
+      res.status(404).send({ error: 'person not found' })
     }
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
-app.get("/info", async (req, res) => {
+app.get('/info', async (req, res, next) => {
   try {
-    const count = await Person.countDocuments({});
+    const count = await Person.countDocuments({})
     const info = `<div>
       <p>PhoneBook has info for ${count} people</p>
       <p>${new Date()}</p>
-    </div>`;
-    res.status(200).send(info);
+    </div>`
+    res.status(200).send(info)
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
-app.post("/api/persons", async (req, res, next) => {
-  const { name, number } = req.body;
+app.post('/api/persons', async (req, res, next) => {
+  const { name, number } = req.body
 
   if (!name || !number) {
-    return res.status(400).json({ error: "Name or number is missing" });
+    return res.status(400).json({ error: 'Name or number is missing' })
   }
 
   try {
-    const existingPerson = await Person.findOne({ name });
+    const existingPerson = await Person.findOne({ name })
     if (existingPerson) {
-      return res.status(409).json({ error: "Name must be unique" });
+      return res.status(409).json({ error: 'Name must be unique' })
     }
 
-    const person = new Person({ name, number });
-    const savedPerson = await person.save();
-    res.status(201).json(savedPerson);
+    const person = new Person({ name, number })
+    const savedPerson = await person.save()
+    res.status(201).json(savedPerson)
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
-app.put("/api/persons/:id", async (req, res) => {
-  const { name, number } = req.body;
+app.put('/api/persons/:id', async (req, res, next) => {
+  const { name, number } = req.body
 
   if (!name || !number) {
-    return errorHandler((error) => next(error));
+    return errorHandler((error) => next(error))
   }
 
   try {
     const updatedPerson = await Person.findByIdAndUpdate(
       req.params.id,
       { name, number },
-      { new: true, runValidators: true, context: "query" }
-    );
+      { new: true, runValidators: true, context: 'query' }
+    )
 
     if (updatedPerson) {
-      res.json(updatedPerson);
+      res.json(updatedPerson)
     } else {
-      res.status(404).send({ error: "person not found" });
+      res.status(404).send({ error: 'person not found' })
     }
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
-app.delete("/api/persons/:id", async (req, res) => {
+app.delete('/api/persons/:id', async (req, res, next) => {
   try {
-    await Person.findByIdAndDelete(req.params.id);
-    res.status(204).end();
+    await Person.findByIdAndDelete(req.params.id)
+    res.status(204).end()
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
-app.use(errorHandler);
-app.use(internalServerError);
+app.use(errorHandler)
+app.use(internalServerError)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- models/person.js
+++ models/person.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
 mongoose
   .connect(url)
-  .then(() => console.log("connected to MongoDB"))
-  .catch((error) => console.log("error connecting to MongoDB:", error.message));
+  .then(() => console.log('connected to MongoDB'))
+  .catch((error) => console.log('error connecting to MongoDB:', error.message))
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -21,26 +21,26 @@ const personSchema = new mongoose.Schema({
     required: true,
 
     set: function (value) {
-      return value.replace(/\s+/g, "");
+      return value.replace(/\s+/g, '')
     },
 
     validate: {
       validator: function (v) {
-        return /^\d{2,3}-\d{7,}$/.test(v);
+        return /^\d{2,3}-\d{7,}$/.test(v)
       },
       message: (props) => `${props.value} is not a valid phone number`,
     },
   },
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-const Person = mongoose.model("Person", personSchema);
+const Person = mongoose.model('Person', personSchema)
 
-module.exports = Person;
+module.exports = Person
